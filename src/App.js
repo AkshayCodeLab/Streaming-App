0fb2f3d52ccd32c0212e16e3fa70a28a5b376ad0
Akshay <akshaygupta089@gmail.com>
2024-07-25 18:37:51 +0530

    Add routes and use axious for fetching

--- src/App.js
+++ src/App.js
@@ -1,22 +1,9 @@
-import { useEffect, useState } from "react";
+import Body from "./Body";
 
 function App() {
-  const [data, setData] = useState("");
-
-  useEffect(() => {
-    makeCall();
-  }, []);
-
-  const makeCall = async () => {
-    const response = await fetch("http://localhost:4000/info");
-    const json = await response.json();
-    setData(json.body);
-  };
-
   return (
     <div>
-      <h2>Data is getting rendered.</h2>
-      {data && <div>{data}</div>}
+      <Body />
     </div>
   );
 }
