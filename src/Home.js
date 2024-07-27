247809d4ee2d826788d0ab7eb6397fbc6bb4d73e
Akshay <akshaygupta089@gmail.com>
2024-07-27 22:01:25 +0530

    Add Home path with token auth

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
+import { useEffect } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
-  return <div>Home</div>;
+  const navigate = useNavigate();
+  const [content, setContent] = useState("");
+
+  console.log(localStorage.getItem("token"));
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    fetchHomePageDetails(token);
+  }, []);
+
+  const fetchHomePageDetails = async (token) => {
+    try {
+      const response = await axios.get(
+        process.env.REACT_APP_BACKEND_URL + "/home",
+        {
+          headers: {
+            Authorization: `${token}`,
+          },
+        }
+      );
+
+      console.log(JSON.stringify(response.data));
+      setContent(response.data?.data);
+    } catch (error) {
+      console.log("This is the error : " + error);
+      localStorage.removeItem("token");
+      navigate("/");
+    }
+  };
+
+  return (
+    <div>
+      <h1>This is the home page.</h1>
+      {content && <div>{content}</div>}
+    </div>
+  );
 };
 
 export default Home;
--- src/Login.js
+++ src/Login.js
@@ -1,7 +1,64 @@
 import React from "react";
-
+import { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 const Login = () => {
-  return <div>Login</div>;
+  const [name, setName] = useState("");
+  const [pass, setPass] = useState("");
+  const [err, setErr] = useState("");
+  const navigate = useNavigate();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("This is the name : " + name);
+    console.log("This is the password : " + pass);
+    console.log(
+      "This is the backend url : " + process.env.REACT_APP_BACKEND_URL
+    );
+
+    login(name, pass);
+  };
+
+  const login = async (name, pass) => {
+    try {
+      const response = await axios.post(
+        process.env.REACT_APP_BACKEND_URL + "/login",
+        {
+          username: name,
+          password: pass,
+        }
+      );
+
+      console.log(
+        "This is the login response : " + JSON.stringify(response.data?.token)
+      );
+      localStorage.setItem("token", response.data?.token);
+      navigate("/Home");
+    } catch (e) {
+      console.log(e.response.data);
+      setName("");
+      setPass("");
+      setErr(e.response.data);
+    }
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          placeholder="username"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <input
+          placeholder="password"
+          value={pass}
+          onChange={(e) => setPass(e.target.value)}
+        />
+        <button type="submit">Submit</button>
+      </form>
+      <div>{err && <div>{err}</div>}</div>
+    </div>
+  );
 };
 
 export default Login;
