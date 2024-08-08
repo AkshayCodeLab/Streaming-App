bac93a59ae360a1897290bb4cba671a12e29fd10
Akshay <akshaygupta089@gmail.com>
2024-08-08 12:41:40 +0530

    Enhance corousal on Home

--- frontend/src/Components/Container.js
+++ frontend/src/Components/Container.js
@@ -6,6 +6,7 @@ const Container = () => {
   const [displayArray, setDisplayArray] = useState(initialDataArr);
   const [ei, setEi] = useState(5);
   const [si, setSi] = useState(data.length - 1);
+  const [trailerUrl, setTrailerUrl] = useState(initialDataArr[2].videoUrl);
   const handleLeft = () => {
     const newDataArr = displayArray.slice(1);
     newDataArr.push(data[ei]);
@@ -21,14 +22,30 @@ const Container = () => {
     setDisplayArray(newDataArr);
   };
   return (
-    <div className="">
-      <TrailerVideo />
-      <button onClick={handleLeft}>{"<"}</button>
-      <button onClick={handleRight}>{">"}</button>
-      <div className="flex">
-        {displayArray.map((element, index) => (
-          <CorousalCard key={index} element={element} />
-        ))}
+    <div className="relative bg-black">
+      <div className="relative z-10">
+        <TrailerVideo trailerUrl={trailerUrl} />
+      </div>
+      <div className="relative -mt-20 z-20">
+        <div className="flex">
+          {displayArray.map((element, index) => (
+            <CorousalCard key={index} element={element} index={index} />
+          ))}
+        </div>
+        <div className="flex justify-between absolute inset-0 z-30">
+          <button
+            className="bg-black text-white flex justify-center items-center w-8 h-full opacity-50 hover:opacity-75"
+            onClick={handleLeft}
+          >
+            {"<"}
+          </button>
+          <button
+            className="bg-black text-white flex justify-center items-center w-8 h-full opacity-50 hover:opacity-75"
+            onClick={handleRight}
+          >
+            {">"}
+          </button>
+        </div>
       </div>
     </div>
   );
@@ -39,36 +56,50 @@ const data = [
     img: "https://res.cloudinary.com/dor9r1mfu/image/upload/v1723020508/john_wick_e3oht3.jpg",
     name: "John Wick",
     type: "Action",
+    videoUrl:
+      "https://res.cloudinary.com/dor9r1mfu/video/upload/v1723021037/Y2meta.app-John_Wick__Chapter_4_2023_Movie_Official_Trailer_Keanu_Reeves_Donnie_Yen_Bill_Skarsg%C3%A5rd_spzhh4.mp4",
   },
   {
     img: "https://res.cloudinary.com/dor9r1mfu/image/upload/v1723024450/A_quiet_place_m3psva.jpg",
     name: "A Quiet Place",
     type: "Horror",
+    videoUrl:
+      "https://res.cloudinary.com/dor9r1mfu/video/upload/v1723099859/Y2meta.app-A_Quiet_Place__Day_One___Final_Trailer_2024_Movie_-_Lupita_Nyong_o_Joseph_Quinn_euhrdm.mp4",
   },
   {
     img: "https://res.cloudinary.com/dor9r1mfu/image/upload/v1723024599/dp3_ibu4qs.jpg",
     name: "Deadpool 3",
     type: "Comedy",
+    videoUrl:
+      "https://res.cloudinary.com/dor9r1mfu/video/upload/v1723099937/Y2meta.app-Deadpool_Wolverine___Nice___In_Theaters_July_26_chjv5l.mp4",
   },
   {
     img: "https://res.cloudinary.com/dor9r1mfu/image/upload/v1723024790/kalki_doweun.avif",
     name: "Kalki",
     type: "Hindi",
+    videoUrl:
+      "https://res.cloudinary.com/dor9r1mfu/video/upload/v1723099976/Y2meta.app-Kalki_2898_AD_-_Blockbuster_Promo___Telugu___Prabhas_Amitabh_Kamal_Haasan_Deepika_Nag_Ashwin_ubyydh.mp4",
   },
   {
     img: "https://res.cloudinary.com/dor9r1mfu/image/upload/v1723025142/dg1rywn-55b55e96-d9f6-4d8f-8b05-a9bb6df05f7b.jpg_lpih0y.jpg",
     name: "Spider Man",
     type: "Animated",
+    videoUrl:
+      "https://res.cloudinary.com/dor9r1mfu/video/upload/v1723100011/Y2meta.app-SPIDER-MAN__ACROSS_THE_SPIDER-VERSE_-_Official_Trailer_HD_j6pmen.mp4",
   },
   {
     img: "https://res.cloudinary.com/dor9r1mfu/image/upload/v1723025220/the-peaky-blinders-shelby-family-yzbar888h96t7ekn_aomrs9.jpg",
     name: "Peaky Blinders",
     type: "Web Series",
+    videoUrl:
+      "https://res.cloudinary.com/dor9r1mfu/video/upload/v1723100207/Peaky_Blinders_Season_6_Official_Trailer___Netflix_India_vhhkxb.mp4",
   },
   {
     img: "https://res.cloudinary.com/dor9r1mfu/image/upload/v1723025356/professor-money-heist-1yegj3ptnd8g5noc_fkfu23.jpg",
     name: "Money Heist",
     type: "Crime",
+    videoUrl:
+      "https://res.cloudinary.com/dor9r1mfu/video/upload/v1723100244/Money_Heist___Series_Trailer___Netflix_j4mekj.mp4",
   },
 ];
 
--- frontend/src/Components/CorousalCard.js
+++ frontend/src/Components/CorousalCard.js
@@ -1,11 +1,18 @@
 import React from "react";
 
-const CorousalCard = ({ element }) => {
-  const { img, name, type } = element;
+const CorousalCard = ({ element, index }) => {
+  const { img, name, type, videoUrl } = element;
+
   return (
-    <div className="relative w-1/5 h-50 shadow-xl ml-2">
+    <div
+      className={`relative w-1/5 h-50 shadow-xl ${index === 0 ? "" : "ml-2"}`}
+    >
       <img src={img} alt={name} className="w-full h-full object-cover" />
-      <div className="absolute inset-0 flex flex-col justify-center items-center text-white text-2xl bg-black bg-opacity-50 hover:bg-opacity-0 transition duration-100 hover:text-xl">
+      <div
+        className={`absolute inset-0 flex flex-col justify-center items-center text-white text-2xl transition duration-100 hover:text-xl ${
+          index === 2 ? "" : "bg-black bg-opacity-60"
+        } hover:bg-opacity-0`}
+      >
         <h2 className="font-bold">{name}</h2>
         <p>{type}</p>
       </div>
